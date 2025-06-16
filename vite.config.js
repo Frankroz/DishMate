8de5eddc261a36c026c672614647d4ac79332f72
Frankroz <franjrm201518@hotmail.com>
2025-06-16 22:35:22 +0800

    Vite.config fixed for production

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        pantry: resolve(__dirname, "src/pantry/index.html"),
+        get_recipes: resolve(__dirname, "src/pantry/get_recipes.html"),
+        recipe_detail: resolve(__dirname, "src/recipe_detail/index.html"),
+        shopping_list: resolve(__dirname, "src/shopping_list/index.html"),
+        recipe_search: resolve(__dirname, "src/recipes/index.html"),
+        saved_recipes: resolve(__dirname, "src//saved_recipes/index.html"),
+        meal_planner: resolve(__dirname, "src//meal_planner/index.html"),
       },
     },
   },
